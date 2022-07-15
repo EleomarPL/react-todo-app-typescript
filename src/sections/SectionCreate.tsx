6de4753418f89d2718b7eff7ab11a122b31601df
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-07-15 12:47:25 -0500

    feat: Base view for the sections

--- src/sections/SectionCreate.tsx
+++ src/sections/SectionCreate.tsx
@@ -1,9 +1,38 @@
 import styled from 'styled-components'
+import { FormEvent, useRef } from 'react'
 
 const SectionCreate = () => {
+  const formRef = useRef<HTMLFormElement>(null)
+
+  const handleAddTask = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault()
+    formRef.current?.reset()
+  }
+
   return (
     <Section>
-      SectionCreate
+      <h1>Crear una nueva tarea</h1>
+      <form className="mb-2 text-start" onSubmit={ handleAddTask }
+        ref={ formRef }
+      >
+        <label htmlFor="newtask" className="form-label">
+          Nueva Tarea
+        </label>
+        <textarea className="form-control"
+          id="newtask" placeholder="Nueva tarea"
+        />
+        <Button>
+          Crear Tarea
+        </Button>
+      </form>
+      <picture>
+        <source
+          srcSet={ new URL('../img/added.webp', import.meta.url) }
+        />
+        <Image src={ new URL('../img/added.png', import.meta.url) }
+          alt="Add Task"
+        />
+      </picture>
     </Section>
   )
 }
@@ -15,6 +44,21 @@ const Section = styled.section`
   padding: 20px;
   color: #3c0272;
   text-align: center;
+`
+const Image = styled.img`
+  width: 80%;
+  object-fit: contain;
+  height: auto;
+  margin-bottom: 20px;
+`
+const Button = styled.button`
+  border: none;
+  border-radius: 10px;
+  margin-top: 1rem;
+  color: #fff;
+  background-color: #562085;
+  padding: 10px 15px;
+  transition: background-color .2s ease-in-out;
 `
 
 export default SectionCreate
--- src/sections/SectionList.tsx
+++ src/sections/SectionList.tsx
@@ -2,8 +2,17 @@ import styled from 'styled-components'
 
 const SectionList = () => {
   return (
-    <Section>
-      SectionList
+    <Section className="px-4">
+      <h2>Tus tareas</h2>
+      <p>Completadas 0 de 0</p>
+      <form className="mb-2 text-start">
+        <label htmlFor="search" className="visually-hidden">
+          Buscar
+        </label>
+        <input type="text" className="form-control"
+          id="search" placeholder="Buscar tarea"
+        />
+      </form>
     </Section>
   )
 }
