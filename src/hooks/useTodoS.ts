2332a3b08bcb3adb7f51e2bff04509774be6b6e0
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-07-17 09:55:27 -0500

    feat: Added status to todo

--- src/hooks/useTodoS.ts
+++ src/hooks/useTodoS.ts
@@ -5,19 +5,23 @@ import { nanoid } from 'nanoid'
 import ContextTodoS from '../contexts/ContextTodoS'
 
 interface Todo {
-  value: string,
-  id: string
+  value: string;
+  id: string;
+  isFinished: boolean;
 }
-interface Person {
+interface TodoCon {
   todoS?: Todo[];
   setTodoS?: (todoS: Todo[]) => void;
 }
 
 const useTodoS = () => {
-  const context: Person = useContext(ContextTodoS)
+  const context: TodoCon = useContext(ContextTodoS)
 
   const addNewTodo = (value: string) => {
-    const newTodoS = [...context.todoS || [], { value, id: nanoid() }]
+    const newTodoS = [
+      ...context.todoS || [],
+      { value, id: nanoid(), isFinished: false }
+    ]
     window.localStorage.setItem('todoS', JSON.stringify(newTodoS))
     if (context.setTodoS) context.setTodoS(newTodoS || [])
   }
